bae5f4da4ce6fb6f2ba63136083008c58dc442bd
gleidsonfagno <gleidsonfagner838@gmail.com>
2025-07-26 19:39:00 -0300

    Create HomePage

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,34 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import {resumes} from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar/>
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Smart feedback for your dream job</h1>
+        <h2>Drop your resume for an ATS score and improvement tips.</h2>
+      </div>
+
+    {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume}/>
+          ))}
+        </div>
+    )}
+    </section>
+
+
+
+  </main>
 }
