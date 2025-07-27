bc4c1da0d7fbdf0ffab4169db137c725b1e34b3d
gleidsonfagno <gleidsonfagner838@gmail.com>
2025-07-27 11:12:34 -0300

    Authentication

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,9 @@ import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
 import {resumes} from "../../constants";
 import ResumeCard from "~/components/ResumeCard";
+import {usePuterStore} from "~/lib/puter";
+import {useLocation, useNavigate} from "react-router";
+import {useEffect} from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,8 +14,19 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const {auth} = usePuterStore()
+
+  const location = useLocation()
+  const navigate = useNavigate();
+
+  // Se o usuário tentar acessar uma rota segura ele sera redirecionado
+  useEffect( () => {
+    if (!auth.isAuthenticated) navigate("/auth?next=/");
+  }, [auth.isAuthenticated])
+
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar/>
+
     <section className="main-section">
       <div className="page-heading">
         <h1>Smart feedback for your dream job</h1>
